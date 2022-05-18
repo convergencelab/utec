846ff8b1ec7c15624d18be8b8abaaeeb6f6db829
James Alexander Hughes <jameshughes89@Gmail.com>
2022-05-18 14:23:05 -0300

    add type hints

--- src/mutations.py
+++ src/mutations.py
@@ -1,8 +1,9 @@
+from collections.abc import Sequence
 from copy import deepcopy
 from random import randrange
 
 
-def bit_flip(chromosome):
+def bit_flip(chromosome: Sequence) -> Sequence:
     """
     Perform a bit flip mutation. This mutation assumes the chromosome is a sequence of bits. Randomly select an index
     within the chromosome and change the value to not value.
@@ -11,7 +12,9 @@ def bit_flip(chromosome):
         1 -> 0
 
     :param chromosome: Individual chromosome to be mutated. Assumed to be a sequence of bits.
+    :type chromosome: Sequence
     :return: Copy of the mutated chromosome
+    :rtype: Sequence
     """
     chromosome = deepcopy(chromosome)
     index = randrange(len(chromosome))
@@ -19,7 +22,7 @@ def bit_flip(chromosome):
     return chromosome
 
 
-def swap_index(chromosome):
+def swap_index(chromosome: Sequence) -> Sequence:
     """
     Select two indices from the chromosome and swap their contents. This mutation is ideal for chromosomes of
     permutations of unique values. It is possible that the same index is selected twice. This situation is ignored and
@@ -29,7 +32,9 @@ def swap_index(chromosome):
             [a, b, c, d, e, f, g] -> [a, b, f, d, e, c, g]
 
     :param chromosome: Individual chromosome to be mutated.
+    :type chromosome: Sequence
     :return: Copy of the mutated chromosome
+    :rtype: Sequence
     """
     chromosome = deepcopy(chromosome)
     index_one = randrange(len(chromosome))
